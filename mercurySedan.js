fa0d5293dbba0a5fca7226effeaa2af500ba7031
Nalinaka <nalinaka@gmail.com>
2023-04-14 20:04:26 -0700

    Finished asssignment

--- mercurySedan.js
+++ mercurySedan.js
@@ -13,23 +13,48 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+   class car extends VehicleModule {
 
+        constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+            this.maximumPassengers = 5;
+            this.passengers = 0;
+            this.numberOfWheels = 4;
+            this.maximumSpeed = 160;
+            this.fuel = 10;
+            this.scheduleService = false;
+        }
 
-
-
-
-
-
-
-
+   }
 
 
 //TO DO: Creating Instances and Testing Them
 
+
+
 //You can use the same instance "v" of the Vehicle class above for the base class.
 
-
-
-
-
 //Create at least two new instances of the Car class and test them here:
+
+
+start() {
+    if (this.fuel > 0) {
+        return this.started = true;
+        console.log("engine started...!!!");
+    } else {
+        return this.started = false;
+        console.log("engine cannot start...");
+    }
+}
+
+
+scheduleService() {
+    if (this.mileage > 30000) {
+        this.scheduleService = true;
+        return this.scheduleService;
+
+    } else {
+        return this.scheduleService = false;
+        
+    }
+}
